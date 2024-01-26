989e551b0b59914b003dc11c723f02860f8dcfeb
Rian Novais <riannovais3@gmail.com>
2024-01-26 11:48:37 -0300

    feat: create "Task" model

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 
 #nesse arquivo teremos configurações do nosso app, e tudo que se relaciona com ele.
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+#config db
+
+db = SQLAlchemy(app)
+
+#config migrate object pra realizar migrações
 
 
 #importando as rotas que irá compor o app
---
+++ models/tables.py
@@ -0,0 +1,24 @@
+from app import db
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    priority = db.Column(db.String, nullable = False)
+    status = db.Column(db.String, nullable = False)
+    startDate = db.Column(db.String, nullable = False)
+
+    def __init__(self, name, priority, status, startDate):
+        self.name = name
+        self.priority = priority
+        self.status = status
+        self.startDate = startDate
+
+        
+        
+
+
+
+
+    
+    
